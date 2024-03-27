6318067e5bde2c342bc45a0c2f1b98c86628c5d6
tomstark99 <tom.stark@r3.com>
2024-03-27 11:00:53 +0000

    updated extension for new button layout

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command === "load_more") {
-        document.getElementsByTagName("button")[3].click();
+        document.querySelector("button.all4-secondary-button.all4-typography-body.all4-episode-list__button").click();
 	    window.scrollTo(0, document.body.scrollHeight);
         sendResponse({result: "success"});
     }
